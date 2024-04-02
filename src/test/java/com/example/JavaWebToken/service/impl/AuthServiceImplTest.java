5317ddbb8fb8b22d9d1e296def4870a4ead7a206
Mirzabek <mirgnv8@gmail.com>
2024-04-02 20:07:11 +0500

    last commit

--- src/test/java/com/example/JavaWebToken/service/impl/AuthServiceImplTest.java
+++ src/test/java/com/example/JavaWebToken/service/impl/AuthServiceImplTest.java
@@ -1,16 +1,11 @@
 package com.example.JavaWebToken.service.impl;
 
-import com.example.JavaWebToken.repository.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
 @ExtendWith(MockitoExtension.class)
 class AuthServiceImplTest {
-    @Mock
-    private UserRepository;
 
     @Test
     void login() {
